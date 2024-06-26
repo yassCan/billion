8b3eb9c5f08b827a7630ba84eaee007dac00d300
yassCan <89553163+yassCan@users.noreply.github.com>
2024-06-26 04:28:02 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.billion',
+  appName: 'billion',
+  webDir: 'dist'
+};
+
+export default config;
